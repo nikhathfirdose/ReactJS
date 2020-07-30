4dea06a997b82afeaeaf18420b040be738626157
nikhathfirdose <nikhathfirdose3@gmail.com>
2020-07-30 19:41:47 +0530

    usage of axios and conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
-import React from "react";
-
-function App() {
-  return (
-    <div className="App">
-      <h1> Nikki</h1>
-    </div>
-  );
+import React, { Component } from "react";
+import axios from "axios";
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: [],
+      loading: false,
+    };
+  }
+  getUsers() {
+    this.setState({
+      loading: true,
+    });
+    axios("https://api.randomuser.me/?nat=US&results=10").then((resp) =>
+      this.setState({
+        users: resp.data.results,
+        loading: false,
+      })
+    );
+  }
+  componentWillMount() {
+    this.getUsers();
+  }
+  render() {
+    return (
+      <div>
+        {!this.state.loading
+          ? this.state.users.map((user) => (
+              <div> {user.name.first + " " + user.name.last}</div>
+            ))
+          : "Its Loading"}
+      </div>
+    );
+  }
 }
-
-export default App;
