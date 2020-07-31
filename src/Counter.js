7717841e41d7689a0f4e7fcf089a02de9c5bf745
nikhathfirdose <nikhathfirdose3@gmail.com>
2020-07-31 16:13:28 +0530

    news app

--- src/Counter.js
+++ src/Counter.js
@@ -1,39 +1,59 @@
-// import React, { Component } from "react";
-
-// export default class Counter extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       count: 0,
-//     };
-//     this.increment = this.increment.bind(this);
-//   }
-//   increment() {
-//     this.setState({
-//       count: this.state.count + 1,
-//     });
-//   }
-//   render() {
-//     return (
-//       <div>
-//         <h1>Counter App</h1>
-//         <button onClick={this.increment}>
-//           Clicked me {this.state.count} times
-//         </button>
-//       </div>
-//     );
-//   }
-// }
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import Loading from "./Loading";
 
 export default function Counter() {
-  const [count, setCount] = useState(0);
+  const [news, setNews] = useState([]);
+  const [search, setSearch] = useState("react");
+  const [url, setUrl] = useState(
+    "https://hn.algolia.com/api/v1/search?query=react"
+  );
+  const message = "Loading.... ";
+  const [loading, setLoading] = useState(false);
+  const getNews = () => {
+    setLoading(true);
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        setNews(data.hits);
+        setLoading(false);
+      })
+      .catch((err) => console.log(err));
+  };
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    //  setSearch(e.target.value);
+    e.preventDefault();
+    setUrl(`https://hn.algolia.com/api/v1/search?query=${search}`);
+  };
+  useEffect(() => {
+    getNews();
+  }, [url]);
+  const searchForm = () => {
+    return (
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={search} onChange={handleChange} />
+        <button>Search</button>
+      </form>
+    );
+  };
+
+  const showNews = () => {
+    return news.map((singleNews, index) => (
+      <div className="app-container" key={index}>
+        <h2 className="news">{singleNews.title}</h2>
+
+        <hr></hr>
+      </div>
+    ));
+  };
   return (
     <div>
-      <h1>Counter App</h1>
-      <button onClick={() => setCount(count + 1)}>
-        Clicked me {count} times
-      </button>
+      <h1> News App</h1>
+      {searchForm()}
+      <h2>{!loading ? showNews() : <Loading message={message} />}</h2>
     </div>
   );
 }
